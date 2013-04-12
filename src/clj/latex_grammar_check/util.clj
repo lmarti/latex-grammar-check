ca0e95c64ad8efa94ea00bae3f9ccda814b93de2
Alexei Guevara <alexguev@gmail.com>
2013-04-12 17:35:23 -0400

    disabled whitespace rule

--- src/clj/latex_grammar_check/util.clj
+++ src/clj/latex_grammar_check/util.clj
@@ -6,12 +6,16 @@
 (defn check-grammar [text]
   (let [lang-tool (JLanguageTool. (BritishEnglish.))]
     (.activateDefaultPatternRules lang-tool)
+    (.disableRule lang-tool "WHITESPACE_RULE")
     (let [matches (.check lang-tool text)]
       (map (fn [m] {:line (.getLine m)
                     :end-line (.getEndLine m)
                     :column (.getColumn m)
                     :end-column (.getEndColumn m)
                     :message (.getMessage m)
+                    :rule-id (.. m getRule getId)
                     :suggested-replacements (vec (.getSuggestedReplacements m))}) matches))))
 
 ;(check-grammar "A sentence with a error in the Hitchhiker's Guide tot he Galaxy")
+
+;(check-grammar "The blue      sea")
